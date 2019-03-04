4e90c0436fd71d649145155a2dd2f9a4a1af3f10
abeeken <abeeken@gmail.com>
2019-03-04 11:00:51 +0000

    Added some functions to play with painitng the squares

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -27,4 +27,20 @@ function rectsPaint(){
     rects.Rect1.path.style.stroke = "lime";
     rects.Rect4.rect.style.fill = "red";
     rects.Rect4.path.style.stroke = "green";
+
+    rects.Rect1.rect.addEventListener("click", function(){
+        if(rects.Rect1.rect.style.fill == "blue"){
+            rects.Rect1.rect.style.fill = "white"; 
+        } else {
+            rects.Rect1.rect.style.fill = "blue";
+        }
+    });
+
+    rects.Rect5.rect.addEventListener("mouseover", function(){
+        rects.Rect5.rect.style.fill = "yellow";
+    });
+
+    rects.Rect5.rect.addEventListener("mouseout", function(){
+        rects.Rect5.rect.style.fill = "white";
+    });
 }
